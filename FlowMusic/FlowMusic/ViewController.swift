6199cc028b96d81bae7ee58ef27b16e371e4dd43
Deokhun KIM <thekoon0456@gmail.com>
2024-03-08 15:01:22 +0900

    [Feat] 프로젝트 설정

---
+++ FlowMusic/FlowMusic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlowMusic
+//
+//  Created by Deokhun KIM on 3/8/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
