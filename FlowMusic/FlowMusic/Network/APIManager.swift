9e85bfd12f9cf76bc88fe288bea4505962d44924
Deokhun KIM <thekoon0456@gmail.com>
2024-03-12 02:55:54 +0900

    [Feat] 프리뷰 재생

--- FlowMusic/FlowMusic/Network/APIManager.swift
+++ FlowMusic/FlowMusic/Network/APIManager.swift
@@ -7,6 +7,8 @@
 
 import Foundation
 
+// MARK: - Test
+
 final class APIManager {
     
     static let shared = APIManager()
--- FlowMusic/FlowMusic/Presentation/ReelsScene/View/ReelsViewController.swift
+++ FlowMusic/FlowMusic/Presentation/ReelsScene/View/ReelsViewController.swift
@@ -7,31 +7,84 @@
 
 import MusicKit
 import UIKit
+import AVFoundation
+import AVKit
+
+import SnapKit
 
 final class ReelsViewController: BaseViewController {
-
+    
     // MARK: - Properties
-
+    
     let musicPlayer = MusicPlayer.shared.player
     private let musicRequest = MusicRequest.shared
     private let viewModel: ReelsViewModel
+    private let musicVideoView = UIView().then {
+        $0.backgroundColor = .systemGray
+    }
     
     // MARK: - Lifecycles
-
+    
     init(viewModel: ReelsViewModel) {
         self.viewModel = viewModel
         super.init()
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        Task {
+            guard let video = try await musicRequest.requestSearchMusicVideoCatalog(term: "조유리").first else { return }
+            print(video)
+            print(video.albumTitle)
+            print(video.albums)
+            print(video.artistName)
+            print(video.artistURL)
+            print(video.moreByArtist)
+            print(video.contentRating)
+            print(video.debugDescription)
+            print(video.description)
+            print(video.duration)
+            print(video.has4K)
+            print(video.hasHDR)
+            print(video.id)
+            print(video.isrc)
+            print(video.playParameters)
+            print(video.previewAssets)
+            print(video.songs)
+            print(video.url)
+            print(video.id)
+            print(video.id)
+            print(video.id)
+            print(video.id)
+            
+            guard let url = video.url?.absoluteURL else { return }
+            print(url)
+            DisplayVideoFromUrl(url: video.previewAssets?.first?.hlsURL, view: musicVideoView)
+        }
     }
-
+    
     override func configureHierarchy() {
+        view.addSubview(musicVideoView)
     }
-
+    
     override func configureLayout() {
+        musicVideoView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+    
+    func DisplayVideoFromUrl(url: URL?, view: UIView) {
+        let player = AVPlayer(url: url!)
+        let playerLayer = AVPlayerLayer(player: player)
+        
+        playerLayer.videoGravity = .resizeAspectFill
+        playerLayer.needsDisplayOnBoundsChange = true
+        playerLayer.frame = view.bounds
+        
+        view.layer.masksToBounds = true
+        view.layer.addSublayer(playerLayer)
+        player.play()
     }
 }
 
